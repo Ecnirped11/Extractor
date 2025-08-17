b0db3c0124b973a0edd2f76f8dd33a9537aefda2
Ecnirped11 <abdulrokibadebisi@gmail.com>
2025-08-17 17:51:07 +0100

    shifting

--- Bot/exhandler.py
+++ Bot/exhandler.py
@@ -4,7 +4,7 @@ import time
 from telegram.constants import ParseMode
 from telegram.ext import CommandHandler, ContextTypes, MessageHandler, filters
 import re
-from .duplicate import DuplicateExtractor
+from .dup_extractor.duplicate import DuplicateExtractor
 
 async def start_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
     username = update.effective_user.username
@@ -13,26 +13,31 @@ async def start_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def handle_txt_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
     document = update.message.document
     caption = update.message.caption
-   
-    if document.mime_type == "text/plain":
-        os.makedirs("downloads", exist_ok=True)
-      
-        file = await context.bot.get_file(document.file_id)  
-        file_path = f"downloads/{document.file_name}"  
-        await file.download_to_drive(file_path)  
-
-        try:  
-            duplicate = DuplicateExtractor(caption ,file_path)
-            response = duplicate.extractor()
+    text = update.message.text
+    
+    if text:
+        await update.message.reply_text(text)
+    if document:
+        if document.mime_type == "text/plain":
+            os.makedirs("downloads", exist_ok=True)
+          
+            file = await context.bot.get_file(document.file_id)  
+            file_path = f"downloads/{document.file_name}"  
+            await file.download_to_drive(file_path)  
+    
+            try:  
+                duplicate = DuplicateExtractor(caption ,file_path)
+                response = duplicate.extractor()
+                
+                await update.message.reply_text(response, parse_mode="HTML")
             
-            await update.message.reply_text(response, parse_mode="HTML")
-        except Exception as e:  
-            await update.message.reply_text(  
-                f"❌ Error reading the file:\n<pre>{e}</pre>", parse_mode=ParseMode.HTML  
-            )
-            os.remove(file_path)
-    else:
-        await update.message.reply_text("❗ Please send a valid .txt file.")
-
+            except Exception as e:  
+                await update.message.reply_text(  
+                    f"❌ Error reading the file:\n<pre>{e}</pre>", parse_mode=ParseMode.HTML  
+                )
+                os.remove(file_path)
+        else:
+            await update.message.reply_text("❗ Please send a valid .txt file.")
+    
 
 
--- __main__.py
+++ __main__.py
@@ -12,7 +12,7 @@ Acceess_token = os.getenv('Token')
 def main():
    app = ApplicationBuilder().token(Acceess_token).build()
    app.add_handler(CommandHandler("start", start_cmd))
-   app.add_handler(MessageHandler(filters.Document.ALL, handle_txt_file))
+   app.add_handler(MessageHandler(filters.TEXT | filters.Document.ALL, handle_txt_file))
    print('bot starting....')
    app.run_polling()
 
